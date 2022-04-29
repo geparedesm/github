9d6a185cdbdc5b18c38356da9289df9c95d28d16
Gabriel Paredes <gabrielparedesmendoza@gmail.com>
2022-04-29 03:24:09 -0500

    [IMP] Show error message and graph the commit tree

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,7 +1,7 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/src/components/tree_component.dart';
 import 'package:flutter_application_1/src/controllers/home_controller.dart';
+import 'package:flutter_application_1/src/models/tree_model.dart';
 import 'package:mvc_pattern/mvc_pattern.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,10 +13,27 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends StateMVC<HomePage> {
   final HomeController _con = HomeController();
+  late TreeModelPainter _painter;
+  TreeModel objTree = TreeModel();
+
   @override
   void initState() {
-    _con.getBranches();
+    _getInfo();
+
     super.initState();
+
+    _painter = TreeModelPainter(objTree);
+  }
+
+  void _getInfo() async {
+    await _con.getBranches();
+    await _con.getCommits();
+    setState(() {
+      objTree.resetTree();
+      for (var item in _con.commitList) {
+        objTree.insertarNode(item);
+      }
+    });
   }
 
   @override
@@ -25,9 +42,32 @@ class _HomePageState extends StateMVC<HomePage> {
       child: Scaffold(
         key: _con.scaffoldMessengerKey,
         appBar: AppBar(
-          title: const Text("Hola"),
+          title: const Center(child: Text("Gabriel Paredes Task")),
         ),
-        body: MaterialButton(onPressed: () async {}, child: const Text("Hola")),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _getInfo();
+            setState(() {});
+          },
+          child: const Icon(Icons.update),
+        ),
+        body: _con.isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : _con.error != ""
+                ? Center(child: Text(_con.error))
+                : SingleChildScrollView(child: graphBranchesAndCommits()),
+      ),
+    );
+  }
+
+  Widget graphBranchesAndCommits() {
+    return SizedBox(
+      height: _con.commitList.length * 50,
+      child: CustomPaint(
+        painter: _painter,
+        size: Size(MediaQuery.of(context).size.width, 250),
       ),
     );
   }
