754a91cefa9baafeed7ac585ac1471990a389eb9
Gabriel Paredes <gabrielparedesmendoza@gmail.com>
2022-04-29 03:23:09 -0500

    [IMP] Get all branches, commits and send error message to the screen

--- lib/src/controllers/home_controller.dart
+++ lib/src/controllers/home_controller.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/src/models/branches_models.dart';
+import 'package:flutter_application_1/src/models/github_commit/github_commit.dart';
+
 import 'package:mvc_pattern/mvc_pattern.dart';
 import '../services/home_services.dart' as services;
 
@@ -9,7 +11,13 @@ class HomeController extends ControllerMVC {
   GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
       GlobalKey<ScaffoldMessengerState>();
   List<BranchesModel> branchesList = [];
+  List<List<GithubCommit>> commitListList = [];
+  List<GithubCommit> commitList = [];
+  bool isLoading = false;
+  String error = "";
   getBranches() async {
+    isLoading = true;
+    branchesList = [];
     final response = await services.getBranches();
     if (response.statusCode == 200) {
       List bodyTemp = jsonDecode(response.body);
@@ -17,8 +25,30 @@ class HomeController extends ControllerMVC {
         branchesList.add(BranchesModel.fromJson(item));
       }
     } else {
-      throw Exception('Failed to load branches');
+      final errorTemp = jsonDecode(response.body);
+
+      error =
+          'Failed to load branches \nError:${response.statusCode} \n${errorTemp['message']}}';
     }
-    // return (x + '2');
+    isLoading = false;
+  }
+
+  getCommits() async {
+    commitList = [];
+    isLoading = true;
+    for (var itemBranch in branchesList) {
+      final response = await services.getCommits(100, itemBranch.name);
+      if (response.statusCode == 200) {
+        List bodyTemp = jsonDecode(response.body);
+        for (var itemCommit in bodyTemp) {
+          final index = branchesList.indexOf(itemBranch);
+          commitList.add(GithubCommit.fromJson(itemCommit,
+              {"branchName": itemBranch.name, "branchIndex": index}));
+        }
+      } else {
+        error = 'Failed to load commits \nError:${response.statusCode}';
+      }
+    }
+    isLoading = false;
   }
 }
