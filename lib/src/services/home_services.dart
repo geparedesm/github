c935305343cc9e8b6eac5c3047cc665db6d148aa
Gabriel Paredes <gabrielparedesmendoza@gmail.com>
2022-04-29 03:21:24 -0500

    [IMP] Service connected to github api

--- lib/src/services/home_services.dart
+++ lib/src/services/home_services.dart
@@ -1,7 +1,28 @@
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
 
 const baseURL = "https://api.github.com/repos/geparedesm/github/";
+const headers = {
+  HttpHeaders.userAgentHeader: 'request',
+  HttpHeaders.acceptHeader: "*/*",
+  HttpHeaders.acceptEncodingHeader: "gzip, deflate, br",
+  HttpHeaders.connectionHeader: "keep-alive",
+  HttpHeaders.authorizationHeader: "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",
+};
+
 Future<http.Response> getBranches() async {
-  final response = await http.get(Uri.parse(baseURL + "branches"));
+  final response = await http.get(
+    Uri.parse(baseURL + "branches"),
+    headers: headers,
+  );
+  return response;
+}
+
+Future<http.Response> getCommits(int maxPages, String branchName) async {
+  final response = await http.get(
+    Uri.parse(baseURL + "commits?per_page=$maxPages&sha=$branchName"),
+    headers: headers,
+  );
   return response;
 }
